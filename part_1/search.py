d4b0b47d3e43bab63612e95fff6592572d889fc2
ZatokaV <zatokav@gmail.com>
2023-01-19 16:27:49 +0200

    Реалізовано пошук цитат по автору (фрагменту) та тегам (фрагменту)

--- part_1/search.py
+++ part_1/search.py
@@ -1,7 +1,7 @@
-from models import Authors, Quotes
+from models import Quotes
 
 
-def pars_input(query):
+def pars_input(query) -> dict:
     list_args = query.split(':')
     field = list_args[0]
     value = list_args[-1].strip().split(',')
@@ -19,7 +19,31 @@ def find_quotes_to_name(query_dict):
             author_name = quote.author.fullname
             all_quotes.append(quote.quote)
 
-    print(author_name, all_quotes)
+    print(f'All quotes by {author_name}: {all_quotes}')
+
+
+def find_quote_to_tag(query_dict):
+    all_quotes = []
+    quotes = Quotes.objects()
+    for quote in quotes:
+        for tag in quote.tags:
+            for part_tag in query_dict['tag']:
+                if tag.startswith(part_tag):
+                    all_quotes.append(f'Quote by {quote.author.fullname} with tag {tag}: {quote.quote}')
+    for result in all_quotes:
+        print(result)
+
+
+def find_quotes_to_tags(query_dict):
+    all_quotes = []
+    quotes = Quotes.objects()
+    for quote in quotes:
+        for tag in quote.tags:
+            for part_tag in query_dict['tags']:
+                if tag.startswith(part_tag):
+                    all_quotes.append(f'Quote by {quote.author.fullname} with tag {tag}: {quote.quote}')
+    for result in all_quotes:
+        print(result)
 
 
 search = True
@@ -31,12 +55,11 @@ while search:
         search = False
 
     else:
-
         query_dict = pars_input(query)
 
         if 'name' in query_dict:
             find_quotes_to_name(query_dict)
         if 'tag' in query_dict:
-            print(query_dict['tag'])
+            find_quote_to_tag(query_dict)
         if 'tags' in query_dict:
-            print(query_dict['tags'])
+            find_quotes_to_tags(query_dict)
