3c16a55cad5c4bbecb9781139c3f1396d68d5786
ZatokaV <zatokav@gmail.com>
2023-01-20 22:33:25 +0200

    Finally refactoring

--- part_1/search.py
+++ part_1/search.py
@@ -7,7 +7,6 @@ client = redis.StrictRedis(host="localhost", port=6379, password=None)
 cache = RedisLRU(client)
 
 
-@cache
 def pars_input(query) -> dict:
     list_args = query.split(':')
     field = list_args[0]
@@ -16,40 +15,26 @@ def pars_input(query) -> dict:
 
 
 @cache
-def find_quotes_to_name(query_dict):
+def find_quotes_to_name(name):
     author_name = ''
     all_quotes = []
-
     quotes = Quotes.objects()
     for quote in quotes:
-        if quote.author.fullname.capitalize() == query_dict['name'][0].capitalize(
-        ) or quote.author.fullname.capitalize().startswith(query_dict['name'][0].capitalize()):
+        if quote.author.fullname.capitalize() == name.capitalize(
+        ) or quote.author.fullname.capitalize().startswith(name.capitalize()):
             author_name = quote.author.fullname
             all_quotes.append(quote.quote)
     return f'All quotes by {author_name}: {all_quotes}'
 
 
 @cache
-def find_quote_to_tag(query_dict):
+def find_quote_to_tag(tag_to_search):
     all_quotes = []
     quotes = Quotes.objects()
     for quote in quotes:
         for tag in quote.tags:
-            for part_tag in query_dict['tag']:
-                if tag.startswith(part_tag):
-                    all_quotes.append(f'Quote by {quote.author.fullname} with tag {tag}: {quote.quote}')
-    return all_quotes
-
-
-@cache
-def find_quotes_to_tags(query_dict):
-    all_quotes = []
-    quotes = Quotes.objects()
-    for quote in quotes:
-        for tag in quote.tags:
-            for part_tag in query_dict['tags']:
-                if tag.startswith(part_tag):
-                    all_quotes.append(f'Quote by {quote.author.fullname} with tag {tag}: {quote.quote}')
+            if tag.startswith(tag_to_search):
+                all_quotes.append(f'Quote by {quote.author.fullname} with tag {tag}: {quote.quote}')
     return all_quotes
 
 
@@ -65,8 +50,13 @@ while search:
         query_dict = pars_input(query)
 
         if 'name' in query_dict:
-            print(find_quotes_to_name(query_dict))
+            for name in query_dict['name']:
+                print(find_quotes_to_name(name))
+
         if 'tag' in query_dict:
-            print(find_quote_to_tag(query_dict))
+            for el in query_dict['tag']:
+                print(find_quote_to_tag(el))
+
         if 'tags' in query_dict:
-            print(find_quotes_to_tags(query_dict))
+            for el in query_dict['tags']:
+                print(find_quote_to_tag(el))
